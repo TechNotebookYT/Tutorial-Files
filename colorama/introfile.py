7bb455602b657e5fcdd585c4b6119adb7531a684
Pranav Bala <technotebook@yahoo.com>
2021-09-18 10:46:20 -0500

    Create introfile.py

---
+++ colorama/introfile.py
@@ -0,0 +1,18 @@
+import colorama
+colorama.init()
+
+print(colorama.Fore.RED + "Hello World")
+print(colorama.Fore.GREEN + "Hello World")
+print(colorama.Fore.BLUE + "Hello World")
+print(colorama.Fore.MAGENTA + "Hello World")
+print(colorama.Fore.YELLOW + "Hello World")
+print(colorama.Fore.CYAN + "Hello World")
+
+print(colorama.Fore.BLACK)
+
+print(colorama.Back.RED + "Hello World" + colorama.Back.RESET)
+print(colorama.Back.GREEN + "Hello World" + colorama.Back.RESET)
+print(colorama.Back.BLUE + "Hello World" + colorama.Back.RESET)
+print(colorama.Back.MAGENTA + "Hello World" + colorama.Back.RESET)
+print(colorama.Back.YELLOW + "Hello World" + colorama.Back.RESET)
+print(colorama.Back.CYAN + "Hello World" + colorama.Back.RESET)
