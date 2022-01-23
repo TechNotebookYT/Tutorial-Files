e4b91530bd273a48bcc928677d175e62ecb47352
Pranav Bala <technotebook@yahoo.com>
2022-01-22 21:22:21 -0600

    Create espconnection.ino

---
+++ serial-mcu-rpi/espconnection.ino
@@ -0,0 +1,42 @@
+#include "Adafruit_VL53L0X.h"
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+void setup() {
+  Serial.begin(115200);
+
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+  
+  Serial.println("Adafruit VL53L0X test");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  // power 
+//  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+}
+
+
+void loop() {
+  VL53L0X_RangingMeasurementData_t measure;
+    
+//  Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("esp_start");
+    Serial.print(measure.RangeMilliMeter - 30);
+    Serial.println("esp_end");
+  }
+  else
+  {
+    Serial.print("esp_start");
+    Serial.print("out of range");
+    Serial.println("esp_end");
+  }
+
+  delay(100);
+}
